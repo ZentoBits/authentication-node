f7dad084e0e64f7026ef2fd21a53419361e74781
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-10-03 15:48:03 -0500

    added some comments for server.js and made user model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+/////// general requires ///////
 const bodyParser = require('body-parser')
 const express = require('express')
 // const session = require('express-session')
@@ -9,12 +10,19 @@ const { connect } = require('./client/db/database')
 
 const app = express()
 
+/*
+  -- Makes a 'const' for the port and sets the default for the server
+*/
 const port = process.env.PORT || 3000
 app.set('port', port)
 app.set('view engine', 'pug')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
+/*
+  -- 'connect()' is connecting to my MongoDB and then listens for interaction with the specified port
+  -- this is where the app is actually started
+*/
 connect()
   .then(() => {
     app.listen(port, () => {
