692c22ec505819fe54eb7a5b7964373e8774c853
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-10-11 15:20:24 -0500

    added comments and cleaned code

--- models/database.js
+++ models/database.js
@@ -1,6 +1,8 @@
 'use strict'
+
+////// General //////
 const  mongoose = require('mongoose')
-  ,   MONGODB_URL = process.env.TEST || 'mongodb://localhost:27017/loginAuth'
+const MONGODB_URL = process.env.TEST || 'mongodb://localhost:27017/loginAuth'
 
 mongoose.Promise = Promise
 
